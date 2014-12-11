14587aa6cd1e9235a96acf80347a0cb9dfd093c2
Koutarou Chikuba <miz404@gmail.com>
2014-12-12 01:54:46 +0900

    Revert "Remove bower"
    This reverts commit 56885527d3426e501fc3d4aa98cb0336245efe87.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require("webpack");
 var path = require('path');
 
 module.exports = {
@@ -12,5 +13,16 @@ module.exports = {
       { test: /\.coffee$/, loader: "coffee" },
       { test: /\.css$/   , loader: "style!css?root=." }
     ]
-  }
+  },
+
+  resolve: {
+    root: [path.join(__dirname, "bower_components")],
+    extensions: ["", ".coffee", ".ts", ".js"]
+  },
+
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    )
+  ]
 }
